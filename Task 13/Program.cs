08f07c59d0b9d4f16ff2cdf8512f668505d4d1da
Eugene <Zenya271998@gmail.com>
2022-08-25 18:13:21 +0600

    dz 2 seminar

---
+++ Task 13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+//Вариант через строку.
+/*
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string StNumber = Convert.ToString(number);
+if(StNumber.Length > 2){
+	Console.WriteLine("Третья цифра числа " + number + " будет: " + StNumber[2]);
+}
+else{
+	Console.WriteLine("Третьей цифры нет.");
+}
+
+*/
+// Вариант через формулу
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99){
+	int LongNumber = (int)Math.Log10(number)+1;
+	int ThirdNum = (number % Convert.ToInt32(Math.Pow(10 , LongNumber - 2))) / Convert.ToInt32(Math.Pow(10 , LongNumber - 3));
+	Console.WriteLine("Третья цифра числа " + number + " будет: " + ThirdNum);
+}
+else{
+	Console.WriteLine("Третьей цифры нет.");
+}
---
+++ Task 15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введи число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 1 || number == 2 || number == 3 || number == 4 || number == 5){
+	Console.WriteLine("Рабочий день");
+}
+else if(number == 6 || number == 7){
+	Console.WriteLine("Выходной день!");
+}
+else{
+	Console.WriteLine("Нет такого дня недели по счету!!!");
+}
