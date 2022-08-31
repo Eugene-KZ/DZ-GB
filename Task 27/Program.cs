24c413aa8000002895572ccce0d5b616adf24550
Eugene <Zenya271998@gmail.com>
2022-08-31 19:59:36 +0600

    dz 4 seminar

---
+++ Task 27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int Summ(string a){
+	int summ = 0;
+	for(int i = 0; i < a.Length; i++){
+		int num = Convert.ToInt32(a[i].ToString());
+		summ += num;
+	}
+	return summ;
+}
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+Console.WriteLine("Сумма цифр числа равна: " + Summ(number));
